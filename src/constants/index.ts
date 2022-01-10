6411a6616cda4c9f319f60b04421e8bd1066d5af
Mayur <mayur.patil@labyrinth.ai>
2022-01-10 12:24:05 +0530

    add file names

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,5 @@
 import path from 'path';
+import fs from 'fs';
 
 export const GetAllObjectsOpts = {
     Bucket: "img-compression",
@@ -9,3 +10,18 @@ export const DuplicateIdsPath = path.resolve('output', 'duplicates.csv');
 export const CONFIG = {
     ITEMS_PER_BATCH_DELETE:  1000
 }
+
+export const Paths = {
+    getInventoryFilePath: (file: string = 'file.csv') => path.resolve('files', 'input', file),
+    getDuplicateMappingsFilePath: () => getOutputFilesPath('duplicate'),
+    getSuccessDeletedPath: () => getOutputFilesPath('deleted'),
+    getErrorDeletedPath: () => getOutputFilesPath('error'),
+}
+
+function getOutputFilesPath(type: string) {
+    fs.mkdirSync(path.resolve('files'));
+    fs.mkdirSync(path.resolve('files', 'output'));
+    fs.mkdirSync(path.resolve('files', 'output', `${type}`));
+    return path.resolve('files', 'output', `${type}`, `${new Date().toISOString()}_${type}.csv`)
+}
+
